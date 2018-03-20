c19aadb78581db07728d24e0e52235393545cf72
robertwlucas <rolucas96@gmail.com>
2018-03-20 13:20:14 -0600

    live camera feed
    cam.py now correctly displays a live feed from the thorlabs camera in a window and then closes it.

--- functions/cam.py
+++ functions/cam.py
@@ -9,21 +9,23 @@
 from instrumental import instrument
 from matplotlib import pyplot
 from instrumental.drivers.cameras import uc480
-
+import cv2
+import time
 paramsets = uc480.list_instruments()
 # Assuming only one camera device is connected, the camera we want to connect
 # to will be the only one in the instrument list.
 
 camera = instrument(paramsets[0])
-a = camera.grab_image()
-pyplot.imshow(a)
-pyplot.show()
+
 #this loop is suposed to update the image live but
 #right now it only updates when the image is closed needs fixed
 
 camera.start_live_video()
 while camera.wait_for_frame():
-    b = camera.latest_frame()
-    pyplot.imshow(b)
-    pyplot.show()
+    frame = camera.latest_frame()
+    cv2.imshow('frame',frame)
+
+    cv2.waitKey(100)
+    if cv2.getWindowProperty('frame',cv2.WND_PROP_VISIBLE) < 1: break
 #I down loaded the source files for instrumental from git hum they are at C:\Users\ecestudent\Downloads\Instrumental-master
+cv2.destroyAllWindows()
