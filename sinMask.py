3581ed5e81c3a456c69c8fec8d2b300fa7a08c70
sequoiap <sequoiap4@gmail.com>
2018-05-23 16:20:54 -0600

    Minor tweaks
    Edited slmpy to allow Python 3 functionality

--- sinMask.py
+++ sinMask.py
@@ -1,12 +1,12 @@
 from functions import slmpy
 import numpy as np
 import time
-from PIL import Image
+#from PIL import Image
 
-def displayVal(seconds, frequency):
+def displayVal(slm, seconds, frequency):
     start = time.time()
     elapsed = 0
-    x = np.empty([600L, 800L, 3L])
+    x = np.empty([600, 800, 3])
     while elapsed < seconds:
         elapsed = time.time() - start
         val = 127.5 * np.sin(2 * np.pi * frequency * elapsed) + 127.5
@@ -15,8 +15,8 @@ def displayVal(seconds, frequency):
         slm.updateArray(screen)
 
 
-slm = slmpy.SLMdisplay(isImageLock = False)
 slm = slmpy.SLMdisplay(monitor = 0)
+#slm = slmpy.SLMdisplay(monitor = 0)
 resX, resY = slm.getSize()
 X,Y = np.meshgrid(np.linspace(0,resX,resX),np.linspace(0,resY,resY))
 #testIMG = np.round((2**8-1)*(0.5+0.5*(-1)*np.sin(2*np.pi*X/50))).astype('uint8')
@@ -24,7 +24,7 @@ X,Y = np.meshgrid(np.linspace(0,resX,resX),np.linspace(0,resY,resY))
 #t = 2 * np.pi * 20 * t
 #display = np.sin(t)
 
-displayVal(30, 2)
+displayVal(slm, 5, 1)
 
 #im = Image.open('Happy_face.jpg')
 #testIMG = np.array(im).astype('uint8')
@@ -34,5 +34,5 @@ displayVal(30, 2)
 #print testIMG
 #print "gotcha"
 #slm.updateArray(testIMG)
-#time.sleep(20)
+time.sleep(2)
 slm.close()
